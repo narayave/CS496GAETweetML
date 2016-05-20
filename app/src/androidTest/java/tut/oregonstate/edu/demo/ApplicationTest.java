1d16fe6b318c7bf4bcd2039df542aa79970516d1
Vedanth Narayanan <vedanth.nd@gmail.com>
2016-05-19 21:51:40 -0700

    Base project

---
+++ app/src/androidTest/java/tut/oregonstate/edu/demo/ApplicationTest.java
@@ -0,0 +1,13 @@
+package tut.oregonstate.edu.demo;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
